e66c42f3c467d7fe7c5619d0d974897edbcd7575
newProject <anhhai19xx@gmail.com>
2020-03-18 15:02:07 +0700

    template engines-pug

--- b1-nodejs/index.js
+++ b1-nodejs/index.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3000;
 
-app.get('/', (req, res) => res.send('<h1>Hello World! </h1>'));
+app.set('view engine', 'pug');
+app.set('views', './views');
 
-app.get('/users', (req, res) => res.send('<h1>user list!</h1>'));
+app.get('/', (req, res) => res.render('index', {
+    name: 'hai',
+}));
+
+app.get('/users', (req, res) => res.render('users', {
+    users: [
+        { id: 1, name: 'hai' },
+        { id: 2, name: 'hao' },
+        { id: 3, name: 'hung' },
+    ]
+}));
 
 app.get('/create', (req, res) => res.send('<h1>Create! </h1>'));
 
